3e7490f01c4feff2395945f9c49f73cccdd3ce09
guruprasadmorbad55 <guruprasadmorbad55@gmail.com>
2024-12-18 20:50:19 +0100

    Add files via upload
    Constants

--- Carbon_footprint_calculation_tool.py
+++ Carbon_footprint_calculation_tool.py
@@ -2,3 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from fpdf import FPDF
 import os
+
+# Constants
+ELECTRICITY_FACTOR = 0.0005
+NATURAL_GAS_FACTOR = 0.0053
+FUEL_FACTOR = 2.32
+WASTE_FACTOR = 0.57
+BUSINESS_TRAVEL_FACTOR = 2.31
+
